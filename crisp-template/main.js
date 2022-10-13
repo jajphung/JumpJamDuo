570986a6688d1dfe8f9d616133f26237bc4287ca
jajphung <102774448+jajphung@users.noreply.github.com>
2022-10-13 12:23:17 -0700

    Add files via upload

---
+++ crisp-template/main.js
@@ -0,0 +1,45 @@
+title = "JUMP";
+
+description = `
+[Tap] Jump
+`;
+
+characters = [
+  `
+lll
+lll
+  `
+];
+
+options = {
+  theme: "dark",
+};
+
+let coins;
+let player;
+
+function update() {
+  if (!ticks) {
+    player = {y: 30, vy: 0};
+  }
+
+  if (input.isJustPressed) {
+    player.vy = -2.5;
+  }
+
+  player.vy += (input.isPressed ? 0.05 : 0.12);
+  player.y += player.vy;
+
+  char("a", 15, player.y);
+
+  if ((player.y < 0) || (player.y > 99)) {
+    play("hit");
+    end();
+  }
+}
+
+addEventListener("load", onLoad);
+
+//https://abagames.github.io/crisp-game-lib/ref_document/modules.html
+//https://github.com/abagames/crisp-game-lib-games/blob/main/docs/intow/main.js
+//https://github.com/OfficialThomas/crisp-template
